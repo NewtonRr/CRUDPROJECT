589ed285be59c27397658f2ff8758ca0800e4aa7
Newton rosa rocha neto <newtonrosa61@gmail.com>
2025-06-24 19:26:12 -0300

    Update frontend components

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,11 @@
+import MainContainer from './mainContainer';
+
+export default function Home() {
+  return (
+    <div>
+      <MainContainer />
+      <h2>Bem vindo ao Sistema de Teste de Cadastro de Clientes</h2>
+      <p> Este é um sistema demonstrativo</p>
+    </div>
+  );
+}
